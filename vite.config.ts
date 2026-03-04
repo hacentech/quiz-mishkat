18e865ffc2b6231bbe1a1aad6202e2b8294aee25
macbook <macbook@macbooks-MacBook-Pro.local>
2026-03-04 17:50:01 +0100

    build: move google script url and mode config to .env files for easy netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,21 @@ import { defineConfig, loadEnv } from 'vite';
 
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.VITE_GAS_URL': JSON.stringify(env.VITE_GAS_URL || 'https://script.google.com/macros/s/AKfycbzgEzU_BTP5f2Ms7tk4b_UOmwwawwiMJ8xBMExoim2aesGtjTuwu6CQ4fhj5QS00WKv/exec'),
-        'process.env.VITE_IS_LOCAL': JSON.stringify(env.VITE_IS_LOCAL !== 'false') // Default to true if not explicitly false
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
